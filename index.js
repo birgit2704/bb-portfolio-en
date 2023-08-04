b95c85428718fdf0dbafb26568a44ccca22207b7
Birgit <birgit.bartenschlager@web.de>
2023-08-04 16:43:26 +0200

    update work section

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ document.querySelector("#btn-cold").addEventListener("click", () => {
   document.body.classList.remove("theme-warm");
 });
 
-var tl = gsap.timeline({ defaults: { duration: 1 } });
+var tl = gsap.timeline({ defaults: { duration: 2.5 } });
 
 tl
   // .from("header", {
@@ -29,8 +29,8 @@ tl
   //   opacity: 0,
   //   delay: 0.5,
   // })
-  .from("#greeting", { opacity: 0 })
-  .from("#f-name", { opacity: 0 }, "-=0.7")
-  .from("#s-name", { opacity: 0 }, "-=0.7")
-  .from(".section__subtitle--intro", { opacity: 0 }, "-=0.7")
-  .from(".intro__img", { y: 50, opacity: 0, ease: "slowMo" }, "-=1");
+  .from("#home", { opacity: 0 });
+// .from("#f-name", { opacity: 0.1 }, "-=0.7")
+// .from("#s-name", { opacity: 0.1 }, "-=0.7")
+// .from(".section__subtitle--intro", { opacity: 0.1 }, "-=0.7")
+// .from(".intro__img", { opacity: 0.1 }, "-=0.7");
