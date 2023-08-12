abeb7b8836453a1008216c46dcb2401199e57ae5
Birgit <birgit.bartenschlager@web.de>
2023-08-12 12:36:42 +0200

    implement warm/cold btn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let preference = +window.localStorage.getItem("preferWarm");
+const button = document.querySelector("#btn-warm");
+
+if (+window.localStorage.getItem("preferWarm")) {
+  document.body.classList.toggle("theme-warm");
+  if (button) {
+    button.textContent = preference ? "⛄" : "☀️";
+  }
+}
+
+//local Storage cannot keep booleans, or integers (therefore '+')
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "btn-warm") {
+    if (+window.localStorage.getItem("preferWarm")) {
+      window.localStorage.setItem("preferWarm", 0);
+      button.textContent = "☀️";
+    } else {
+      window.localStorage.setItem("preferWarm", 1);
+      button.textContent = "⛄";
+    }
+    document.body.classList.toggle("theme-warm");
+  }
+});
+
+// NAVIGATION
+
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
 
@@ -11,25 +38,17 @@ navLinks.forEach((link) =>
   })
 );
 
-document.querySelector("#btn-warm").addEventListener("click", () => {
-  document.body.classList.add("theme-warm");
+// ANIMATION WHEN PAGE LOADS
 
-  //   document.querySelector("#btn-warm").disabled = true;
-});
+// var tl = gsap.timeline({ defaults: { duration: 2.5 } });
 
-document.querySelector("#btn-cold").addEventListener("click", () => {
-  document.body.classList.remove("theme-warm");
-});
-
-var tl = gsap.timeline({ defaults: { duration: 2.5 } });
-
-tl
-  // .from("header", {
-  //   y: 50,
-  //   opacity: 0,
-  //   delay: 0.5,
-  // })
-  .from("#home", { opacity: 0 });
+// tl
+// .from("header", {
+//   y: 50,
+//   opacity: 0,
+//   delay: 0.5,
+// })
+// .from("#home", { opacity: 0 });
 // .from("#f-name", { opacity: 0.1 }, "-=0.7")
 // .from("#s-name", { opacity: 0.1 }, "-=0.7")
 // .from(".section__subtitle--intro", { opacity: 0.1 }, "-=0.7")
